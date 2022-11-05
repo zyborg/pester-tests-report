7a241aecbc791ff766fade5f6a9c4b8183e88614
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-11-05 11:24:58 -0400

    Change 'utf-8' to 'utf8' for Windows PowerShell compatability

--- action.ps1
+++ action.ps1
@@ -157,20 +157,20 @@ else {
     Export-Clixml -InputObject $pesterResult -Path $result_clixml_path
 
     if ($error_message) {
-        "error_message=$error_message" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
+        "error_message=$error_message" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
     }
     if ($error_clixml_path) {
-        "error_clixml_path=$error_clixml_path" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
+        "error_clixml_path=$error_clixml_path" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
     }
     if ($inputs.tests_fail_step -and ($pesterResult.FailedCount -gt 0)) {
         $script:stepShouldFail = $true
     }
 
-    "result_clixml_path=$result_clixml_path" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
-    "result_value=$($pesterResult.Result)" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
-    "total_count=$($pesterResult.TotalCount)" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
-    "passed_count=$($pesterResult.PassedCount)" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
-    "failed_count=$($pesterResult.FailedCount)" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
+    "result_clixml_path=$result_clixml_path" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
+    "result_value=$($pesterResult.Result)" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
+    "total_count=$($pesterResult.TotalCount)" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
+    "passed_count=$($pesterResult.PassedCount)" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
+    "failed_count=$($pesterResult.FailedCount)" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
 }
 
 function Resolve-EscapeTokens {
@@ -442,14 +442,14 @@ function Publish-ToGist {
 }
 
 if ($test_results_path) {
-    "test_results_path=$test_results_path" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
+    "test_results_path=$test_results_path" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
 
     Build-MarkdownReport
 
     $reportData = [System.IO.File]::ReadAllText($test_report_path)
 
     if ($coverage_results_path) {
-        "coverage_results_path=$coverage_results_path" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
+        "coverage_results_path=$coverage_results_path" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
 
         Build-CoverageReport
 
