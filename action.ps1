d97e41993083398f99182f41daa573757ccecf0e
Nate Scherer <376408+natescherer@users.noreply.github.com>
2023-10-27 04:50:42 -0400

    Load System.Web before it is used for 5.1 compatability

--- action.ps1
+++ action.ps1
@@ -206,6 +206,7 @@ function Resolve-EscapeTokens {
     $m += $Message.Substring($p2 + 1)
 
     if ($UrlEncode) {
+        Add-Type -AssemblyName System.Web
         $m = [System.Web.HTTPUtility]::UrlEncode($m).Replace('+', '%20')
     }
 
